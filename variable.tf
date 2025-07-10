b6af00a8724dc5343def985d4b96f9943b514ccc
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-07-10 19:42:00 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,65 +1,70 @@
-variable "project_id" {}
-variable "region" {}
-variable "network" {}
-variable "subnetwork" {}
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "network" {
+  description = "VPC network name or self_link"
+  type        = string
+}
+
+variable "subnetwork" {
+  description = "Subnetwork name or self_link"
+  type        = string
+}
 
 variable "use_existing_sa" {
-  type    = bool
-  default = false
+  description = "Set to true to use an existing service account"
+  type        = bool
+  default     = false
 }
 
 variable "service_account_email" {
-  type    = string
-  default = ""
+  description = "Email of existing service account (used if use_existing_sa = true)"
+  type        = string
+  default     = ""
 }
 
 variable "service_account_id" {
-  type    = string
-  default = "gke-sa"
+  description = "ID for new service account (used if use_existing_sa = false)"
+  type        = string
+  default     = "gke-service-account"
 }
 
 variable "service_account_roles" {
-  type    = list(string)
+  description = "IAM roles to attach to GKE service account"
+  type        = list(string)
   default = [
     "roles/container.nodeServiceAccount",
-    "roles/compute.networkViewer"
+    "roles/compute.instanceAdmin.v1",
+    "roles/iam.serviceAccountUser"
   ]
 }
 
-variable "cluster" {
-  type = object({
-    name                  = string
-    location              = string
-    enable_private_nodes  = optional(bool)
-    master_ipv4_cidr_block = optional(string)
-    default_node_config = object({
-      machine_type = string
-      disk_size_gb = number
-      disk_type    = string
-    })
-  })
-}
-
-variable "node_pools" {
+variable "clusters" {
+  description = "Map of GKE cluster configurations"
   type = map(object({
-    initial_node_count = optional(number)
-    min_node_count     = optional(number)
-    max_node_count     = optional(number)
-    node_config = object({
-      machine_type        = string
-      disk_size_gb        = number
-      disk_type           = string
-      spot                = optional(bool)
-      labels              = optional(map(string))
-      taints              = optional(list(object({
+    name                   = string
+    location               = string
+    autopilot              = bool
+    enable_private_nodes   = optional(bool, false)
+    master_ipv4_cidr_block = optional(string)
+    initial_node_count     = optional(number, 1)
+
+    node_pools = optional(map(object({
+      node_count     = optional(number, 1)
+      min_node_count = optional(number, 1)
+      max_node_count = optional(number, 3)
+      machine_type   = string
+      disk_size_gb   = number
+      disk_type      = string
+      spot           = optional(bool, false)
+      labels         = optional(map(string), {})
+      taints = optional(list(object({
         key    = string
         value  = string
         effect = string
-      })))
-      guest_accelerators  = optional(list(object({
-        type  = string
-        count = number
-      })))
-    })
+      })), [])
+    })), {})
   }))
 }
