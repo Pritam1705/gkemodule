bcd0995d59756e5a69034b487b1b7e3d5adbe4d5
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-07-10 19:44:36 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,1 @@
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
 
-output "cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
-}
-
-output "node_pools" {
-  value = [for np in google_container_node_pool.nodepools : np.name]
-}
