ff8e46152a4fdf3262aa107666e7cee1b6faeca9
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-07-11 20:12:36 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,14 @@
+output "standard_cluster_names" {
+  description = "Names of the standard (non-autopilot) GKE clusters"
+  value       = [for k, v in google_container_cluster.standard : v.name]
+}
 
+output "autopilot_cluster_names" {
+  description = "Names of the autopilot GKE clusters"
+  value       = [for k, v in google_container_cluster.autopilot : v.name]
+}
+
+output "standard_node_pool_names" {
+  description = "Names of all standard node pools"
+  value       = [for k, v in google_container_node_pool.standard_nodepool : v.name]
+}
