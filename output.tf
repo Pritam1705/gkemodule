51b2829c3cc44789e9df4901ccfcdfb0e17b9d28
Pritam1705 <kondapratiwarpritam855@gmail.com>
2025-07-11 20:58:17 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,19 +1,17 @@
 output "standard_cluster_names" {
-  value = [for c in google_container_cluster.standard : c.name]
+  value = [for k, v in google_container_cluster.standard : v.name]
 }
 
 output "autopilot_cluster_names" {
-  value = [for c in google_container_cluster.autopilot : c.name]
+  value = [for k, v in google_container_cluster.autopilot : v.name]
 }
 
-output "cluster_endpoints" {
-  value = {
-    for k, c in google_container_cluster.standard : k => c.endpoint
-  }
+output "standard_node_pool_names" {
+  value = [for k, v in google_container_node_pool.standard_nodepool : v.name]
 }
 
-output "autopilot_endpoints" {
-  value = {
-    for k, c in google_container_cluster.autopilot : k => c.endpoint
-  }
+output "service_account_email" {
+  value = var.use_existing_sa ? var.service_account_email : (
+    length(google_service_account.gke_sa) > 0 ? google_service_account.gke_sa[0].email : null
+  )
 }
